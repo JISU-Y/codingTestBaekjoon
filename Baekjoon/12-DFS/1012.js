a9320297d1ac15f9be4e864ef6044b459da32a16
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2024-01-04 00:30:40 +0900

    boj: binary search

--- Baekjoon/12-DFS/1012.js
+++ Baekjoon/12-DFS/1012.js
@@ -30,14 +30,59 @@ let caseList = cases.map((el) => el.split(" ").map((e) => Number(e)))
 
 let M, N, K
 for (let i = 0; i < T; i++) {
+  let worm = 1
   ;[M, N, K] = caseList[0]
+
+  let ground = new Array(N).fill(0).map(() => new Array(M).fill(0))
 
   caseList.shift()
 
   for (let j = 0; j < K; j++) {
     let [X, Y] = caseList[j]
-    console.log([X, Y])
+
+    ground[Y][X] = 1
   }
 
+  ground.forEach((cabRow, rowIndex) => {
+    cabRow.forEach((cab, colIndex) => {
+      if (cab === 1) {
+        dfs([colIndex, rowIndex], ground)
+        // console.log("ground!!", ground)
+        worm++
+      }
+    })
+  })
+
+  //   console.log(ground)
+  //   console.log(worm)
   caseList = caseList.slice(K)
+}
+
+function dfs([x, y], visited) {
+  let neighbors = [
+    [0, 1], // 오른쪽
+    [1, 0], // 아래쪽
+    [0, -1], // 왼쪽
+    [-1, 0], // 위쪽
+  ]
+    .map(([posX, posY]) => {
+      const [movedX, movedY] = [x + posX, y + posY]
+      if (movedX >= 0 && movedY >= 0 && movedX < M && movedY < N) {
+        return [movedX, movedY]
+      }
+    })
+    .filter((el) => el && visited[el[1]][el[0]] === 1 && visited[el[1]][el[0]] !== 2)
+
+  visited[y][x] = 2
+  let result = [[x, y]]
+
+  console.log(neighbors)
+
+  neighbors.forEach((neighbor) => {
+    if (visited[y][x] !== 2) {
+      result.push(...dfs(neighbor, visited))
+    }
+  })
+
+  return result
 }
---
+++ Baekjoon/14-Binary-Search/2512.js
@@ -0,0 +1,82 @@
+/* 예산 */
+/* ! 테스트 */
+const path = "./input.txt"
+const fs = require("fs")
+
+const readline = require("readline")
+const rl = readline.createInterface({
+  input: fs.createReadStream(path),
+  output: process.stdout,
+})
+
+// /* ! 제출용 */
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+
+let input = []
+
+rl.on("line", function (line) {
+  input.push(line.split(" "))
+}).on("close", function () {
+  const [N] = input.shift()
+  const budgetList = input.shift().map((e) => +e)
+  const [budgetMaxLimit] = input.shift()
+
+  console.log("N", N)
+  console.log("budgetList", budgetList)
+  console.log("budgetMaxLimit", budgetMaxLimit)
+
+  const MIN = 1
+  const MAX = Math.max(...budgetList)
+
+  // console.log("min?", MIN)
+  // console.log("max?", MAX)
+
+  let sortedList = []
+  for (let i = MIN; i <= MAX; i++) {
+    sortedList.push(i)
+  }
+
+  console.log(binarySearch(sortedList, budgetList, budgetMaxLimit))
+
+  process.exit()
+})
+
+const binarySearch = (arr, targetArr, targetValue) => {
+  let low = 0
+  let high = arr.length - 1
+  let realResult = 0
+
+  while (low <= high) {
+    let middle = Math.floor((low + high) / 2)
+    const current = arr[middle]
+
+    const curArr = targetArr.map((el) => (el >= current ? current : el))
+    let curResult = curArr.reduce((a, b) => a + b, 0)
+
+    console.log("curArr", curArr)
+    console.log("curResult", curResult)
+    // 120 110 140 150
+    // 70
+    // 50 40 70 70 => 230
+    // 100
+    // 100 100 100 100 => 400 (85 남음)
+    // 127
+    // 120 110 127 127 => 484 (1남음)
+    // 128
+    // 120 110 128 128 => 486 (1 모자름)
+
+    // 나눠 준 것보다 돈이 많이 남는지 아닌지 판단
+    if (targetValue < curResult) {
+      high = middle - 1
+    } else if (targetValue >= curResult) {
+      low = middle + 1
+      realResult = low // !!!!!! 조건을 만족하는 값 저장하기!
+    }
+  }
+
+  return realResult
+}
